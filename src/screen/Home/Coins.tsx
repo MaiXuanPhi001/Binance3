789b5af889c8a154aa632a34641dc9248fee0d86
Mai Xuân Phi <101462441+MaiXuanPhi001@users.noreply.github.com>
2023-10-03 16:43:01 +0700

    abc
    abc

--- src/screen/Home/Coins.tsx
+++ src/screen/Home/Coins.tsx
@@ -1,29 +1,26 @@
-import { useAppSelector } from '@hooks/index'
-import { coinsFuturesChartSelector } from '@selector/futuresSelector'
+import contants from '@util/contants'
 import React, { useEffect, useState } from 'react'
 import { useSharedValue } from 'react-native-reanimated'
-import CoinsAnimated from './CoinsAnimated'
 import { io } from 'socket.io-client'
-import contants from '@util/contants'
 import { ICoins } from 'src/model/futuresModel'
+import CoinsAnimated from './CoinsAnimated'
 
 const Coins = () => {
+    const [data, setData] = useState<ICoins[]>([])
     const coins = useSharedValue<ICoins[]>([])
-    const [render, setRendet] = useState(0)
 
     useEffect((): any => {
         const newSocket = io(contants.HOSTING)
 
         newSocket.on('listCoin', (coinsSocket: ICoins[]) => {
             if (coinsSocket) {
-                coins.value = [{ close: Math.random() }, { close: Math.random() }]
-                setRendet(render + 1)
+                setData([{ close: Math.random() }, { close: Math.random() }])
             }
         })
 
         return () => newSocket.disconnect()
-    }, [render])
-
+    }, [])
+    coins.value = data
     return (
         <CoinsAnimated
             coins={coins}
--- src/screen/Home/CoinsAnimated.tsx
+++ src/screen/Home/CoinsAnimated.tsx
@@ -1,38 +1,35 @@
-import { View, Text } from 'react-native'
-import React, { memo } from 'react'
 import Box from '@commom/Box'
 import Txt from '@commom/Txt'
-import { colors } from '@theme/colors'
-import { useTranslation } from 'react-i18next'
-import { ICoins } from 'src/model/futuresModel'
-import CoinItem from './CoinItem'
 import { useAppDispatch, useTheme } from '@hooks/index'
-import futuresSlice from '@slice/futuresSlice'
 import { navigate } from '@navigation/navigationRef'
+import futuresSlice from '@slice/futuresSlice'
+import { colors } from '@theme/colors'
 import { screen } from '@util/screens'
+import React, { memo } from 'react'
+import { useTranslation } from 'react-i18next'
+import Animated, { useAnimatedStyle } from 'react-native-reanimated'
+import { ICoins } from 'src/model/futuresModel'
 import { Coin } from 'src/model/tradeModel'
-import Animated, { useAnimatedProps, useAnimatedStyle, useDerivedValue } from 'react-native-reanimated'
-import { TextInput } from 'react-native-gesture-handler'
-import { ReText } from 'react-native-redash'
 
-const InputAnimated = Animated.createAnimatedComponent(TextInput)
 
 const CoinsAnimated = ({ coins }: any) => {
-    const theme = useTheme()
-    const { t } = useTranslation()
-    const dispatch = useAppDispatch()
+    // const theme = useTheme()
+    // const { t } = useTranslation()
+    // const dispatch = useAppDispatch()
 
-    const handleMoveTrade = (coin: Coin) => {
-        dispatch(futuresSlice.actions.setSymbol({
-            symbol: coin.symbol,
-            currency: coin.currency,
-        }))
-        navigate(screen.TRADE)
-    }
+    // const handleMoveTrade = (coin: Coin) => {
+    //     dispatch(futuresSlice.actions.setSymbol({
+    //         symbol: coin.symbol,
+    //         currency: coin.currency,
+    //     }))
+    //     navigate(screen.TRADE)
+    // }
+
+    console.log('render')
 
     return (
         <Box marginTop={20}>
-            <Box row alignCenter justifySpaceBetween>
+            {/* <Box row alignCenter justifySpaceBetween>
                 <Txt color={colors.gray2} size={12}>{t('Name')}</Txt>
                 <Box row >
                     <Txt color={colors.gray2} size={12}>{t('Last Price')}</Txt>
@@ -46,23 +43,22 @@ const CoinsAnimated = ({ coins }: any) => {
                         </Txt>
                     </Box>
                 </Box>
-            </Box>
-            <Animated.View>
-                {coins.value.map((coin: ICoins, index: number) => {
-                    return (
-                        <Animated.View
-                            style={useAnimatedStyle(() => {
-                                return {
-                                    height: 20,
-                                    width: coins.value[index].close,
-                                    backgroundColor: 'red',
-                                    marginTop: 10,
-                                }
-                            })}
-                        />
-                    )
-                })}
-            </Animated.View>
+            </Box> */}
+            {coins.value.map((coin: ICoins, index: number) => {
+                return (
+                    <Animated.View
+                        key={index}
+                        style={useAnimatedStyle(() => {
+                            return {
+                                height: 20,
+                                width: coins.value[index].close,
+                                backgroundColor: 'red',
+                                marginTop: 10,
+                            }
+                        })}
+                    />
+                )
+            })}
         </Box >
     )
 }
